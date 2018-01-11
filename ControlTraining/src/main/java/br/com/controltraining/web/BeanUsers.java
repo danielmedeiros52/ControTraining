2fc1b4467c8c37a40a8702a5dfe4c03d8c0c24f4
Daniel <danielmedeiros52@gmail.com>
2018-01-11 15:10:38 -0300

    listar usuarios

--- ControlTraining/src/main/java/br/com/controltraining/web/BeanUsers.java
+++ ControlTraining/src/main/java/br/com/controltraining/web/BeanUsers.java
@@ -1,66 +1,70 @@
-
 package br.com.controltraining.web;
 
 import br.com.controltraining.users.Users;
 import br.com.controltraining.users.UsersBO;
+import java.util.List;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 
-
-
 @ManagedBean(name = "BeanUsers")
 @RequestScoped
 public class BeanUsers {
-    
-    private Users usuario = new Users ();
-	private String confirmarSenha;
 
-        public String listar() {
-		this.usuario = new Users();
-		this.usuario.setAtivo(true);
+    private Users usuario = new Users();
+    private String confirmarSenha;
+    private List<Users> lista;
+
+    public List<Users> listar() {
+        if (this.lista == null) {
+            UsersBO usersBO = new UsersBO();
+            this.lista = usersBO.listar();
+        }
+
+        return this.lista;
+    }
+
+    public String users() {
+		
 		return "/publico/users";
-	}
-        
-        
-        
-	public String cadastrar() {
-		this.usuario = new Users();
-		this.usuario.setAtivo(true);
-		return "/publico/cadUsers";
-	}
+    }
+    public String cadastrar() {
+        this.usuario = new Users();
+        this.usuario.setAtivo(true);
+        return "/publico/cadUsers";
+    }
 
-	public String salvar() {
-		FacesContext context = FacesContext.getCurrentInstance();
+    public String salvar() {
+        FacesContext context = FacesContext.getCurrentInstance();
 
-		String senha = this.usuario.getSenha();
-		if (!senha.equals(this.confirmarSenha)) {
-			FacesMessage facesMessage = new FacesMessage("A senha nao foi confirmada corretamente");
-			context.addMessage(null, facesMessage);
-			return null;
-		}
+        String senha = this.usuario.getSenha();
+        if (!senha.equals(this.confirmarSenha)) {
+            FacesMessage facesMessage = new FacesMessage("A senha nao foi confirmada corretamente");
+            context.addMessage(null, facesMessage);
+            return null;
+        }
 
-		UsersBO UsersBO = new UsersBO();
-		UsersBO.salvar(this.usuario);
+        UsersBO UsersBO = new UsersBO();
+        UsersBO.salvar(this.usuario);
 
-		return "usuariosucesso";
-	}
+        return "usuariosucesso";
+    }
 
-	public Users getUsuario() {
-		return usuario;
-	}
+    public Users getUsuario() {
+        return usuario;
+    }
 
-	public void setUsuario(Users usuario) {
-		this.usuario = usuario;
-	}
+    public void setUsuario(Users usuario) {
+        this.usuario = usuario;
+    }
 
-	public String getConfirmarSenha() {
-		return confirmarSenha;
-	}
+    public String getConfirmarSenha() {
+        return confirmarSenha;
+    }
 
-	public void setConfirmarSenha(String confirmarSenha) {
-		this.confirmarSenha = confirmarSenha;
-	}
-    
+    public void setConfirmarSenha(String confirmarSenha) {
+        this.confirmarSenha = confirmarSenha;
+    }
+
 }
