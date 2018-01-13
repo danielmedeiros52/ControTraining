867f05d08c786c250ceb174511628bb338064e11
Daniel Medeiros <danielmedeiros52@gmail.com>
2018-01-13 19:50:01 -0300

    layout CadUsers 2

--- ControlTraining/src/main/webapp/resources/js/scripts.js
+++ ControlTraining/src/main/webapp/resources/js/scripts.js
@@ -1,5 +1,17 @@
- $(document).ready(function () {
-                $("#menu").click(function () {
-                    $("#menuContent").slideToggle("slow");
-                });
-            });
+$(document).ready(function () {
+    $("#menu").click(function () {
+        $("#menuContent").slideToggle("slow");
+    });
+});
+$(document).ready(function () {
+    $('select').material_select();
+});
+
+$('#datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15, // Creates a dropdown of 15 years to control year,
+    today: 'Today',
+    clear: 'Clear',
+    close: 'Ok',
+    closeOnSelect: false // Close upon selecting a date,
+});
